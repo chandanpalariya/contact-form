d4c3f5f4bc868deec2989eeb590a97bf7ea88054
chandan_palariya <165251772+chandanpalariya@users.noreply.github.com>
2026-01-03 13:59:52 +0530

    Remove fallback for API_URL in api.js

--- frontent/src/config/api.js
+++ frontent/src/config/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || "https://contact-form1-l5qc.onrender.com/";
+const API_URL =  "https://contact-form1-l5qc.onrender.com";
 
 export const apiConfig = {
   baseURL: API_URL,
